8dcbed4b50f54e4a08b58acbc709b26d4907b26e
Tal Williams <polygonchorus@gmail.com>
2017-07-20 16:12:09 -0700

    Add displayName of wrapped component to the Connected component

--- src/connect.js
+++ src/connect.js
@@ -1,27 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const connect = (mapProps = (p => p)) => Component => {
+const connect = (mapProps = p => p) => Component => {
   class Connected extends React.Component {
-    render () {
+    render() {
       const mapped = mapProps(this.context.state)
 
       return (
-        <Component
-          {...this.props}
-          {...mapped}
-          update={this.context.update}
-        />
+        <Component {...this.props} {...mapped} update={this.context.update} />
       )
     }
   }
 
+  Connected.displayName = `Connected(${getDisplayName(Component)})`
   Connected.contextTypes = {
     update: PropTypes.func.isRequired,
-    state: PropTypes.object.isRequired
+    state: PropTypes.object.isRequired,
   }
 
   return Connected
 }
+
+const getDisplayName = Component =>
+  Component.displayName || Component.name || 'Component'
 
 export default connect

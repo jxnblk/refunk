8d993d96ea98c8a3a20ed0b9019e84600fdd9f5e
Brent Jackson <jxnblk@gmail.com>
2018-04-09 10:32:06 -0400

    Update example

--- examples/Counter.js
+++ examples/Counter.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import connect from 'refunk'
+import { connect } from '..'
 import { dec, inc } from './updaters'
 
-const Counter = (props, context) => (
+const Counter = props => (
   <div id='counter'>
     <h2>Count: {props.count}</h2>
     <button onClick={e => props.update(dec)} children='-' />
